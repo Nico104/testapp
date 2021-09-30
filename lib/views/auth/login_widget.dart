ce7c18901ca0b0c4ed28de836253b2aef3fe6eb0
Nico104 <nicolascarraro104@gmail.com>
2021-09-30 13:35:06 +0200

    s

---
+++ lib/views/auth/login_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:testsite/utils/animated_progress_indicator_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: LoginForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _usernameTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+
+  double _formProgress = 0;
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _usernameTextController,
+      _passwordTextController
+    ];
+
+    for (final controller in controllers) {
+      if(controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      onChanged: _updateFormProgress, // NEW
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AnimatedProgressIndicator(value: _formProgress),
+          Text('Login', style: Theme
+              .of(context)
+              .textTheme
+              .headline4),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _usernameTextController,
+              decoration: const InputDecoration(hintText: 'Username'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _passwordTextController,
+              decoration: const InputDecoration(hintText: 'Password'),
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+                return states.contains(MaterialState.disabled) ? null : Colors.white;
+              }),
+              backgroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+                return states.contains(MaterialState.disabled) ? null : Colors.blue;
+              }),
+            ),
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
+            child: const Text('Login'),
+          ),
+        ],
+      ),
+    );
+  }
+}
